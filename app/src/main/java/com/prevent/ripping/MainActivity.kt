94133b9a653dfa9761a422e378e8835bbff89e78
fumiya-kume <fumiya.kume@hotmail.com>
2019-12-03 20:43:43 +0900

    add faeture of show google map

--- app/src/main/java/com/prevent/ripping/MainActivity.kt
+++ app/src/main/java/com/prevent/ripping/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.prevent.ripping
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.prevent.alertmap.AlertMapFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        activity_main_navigate_to_map_button.setOnClickListener {
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(
+//                    R.id.activity_main_container_fragment,
+//                    AlertMapFragment()
+//                )
+//                .commitNow()
+//        }
     }
 }
