1f505606388a389666d0eb2e5414f96dd8f90d4a
Fumiya Kume <fumiya.kume@hotmail.com>
2019-12-13 09:34:05 +0900

    Refactoring/reformat code (#33)
    * reformat code
    
    * Refactoring of map activity

---
+++ feature/alertmap/src/main/java/com/prevent/alertmap/service/MapServiceImpl.kt
@@ -0,0 +1,29 @@
+package com.prevent.alertmap.service
+
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.prevent.alertmap.entity.CurrentLocationEntity
+
+internal class MapServiceImpl(
+    private val fragment: Fragment
+) : MapService {
+
+    override fun ZoomInTolocation(locationEntity: CurrentLocationEntity) {
+        if (fragment is SupportMapFragment) {
+            val supportMapFragment = fragment as SupportMapFragment
+            supportMapFragment.getMapAsync {
+                it.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            locationEntity.location.lat.value,
+                            locationEntity.location.lon.value
+                        ),
+                        15F
+                    )
+                )
+            }
+        }
+    }
+}
