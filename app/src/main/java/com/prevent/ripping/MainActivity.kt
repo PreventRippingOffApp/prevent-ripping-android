c2e427122e9042610112058eca01dab7b866fac8
Fumiya Kume <fumiya.kume@hotmail.com>
2019-12-17 20:50:37 +0900

    Feature/setting (#38)
    * add setting module
    
    * update build.gradle
    
    * install preference library
    
    * add setting feature base

--- app/src/main/java/com/prevent/ripping/MainActivity.kt
+++ app/src/main/java/com/prevent/ripping/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.prevent.ripping
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.prevent.feature.setting.PreferenceActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(
+            Intent(
+                applicationContext,
+                PreferenceActivity::class.java
+            )
+        )
     }
 }
