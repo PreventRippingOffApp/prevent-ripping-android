469ba6b16548818ff8746e17704146face254e2c
fumiya-kume <fumiya.kume@hotmail.com>
2019-12-17 20:50:05 +0900

    add setting feature base

---
+++ feature/setting/src/main/java/com/prevent/feature/setting/PreferenceActivity.kt
@@ -0,0 +1,7 @@
+package com.prevent.feature.setting
+
+import androidx.fragment.app.FragmentActivity
+
+class PreferenceActivity : FragmentActivity(R.layout.activity_preference) {
+
+}
---
+++ feature/setting/src/main/java/com/prevent/feature/setting/PreferenceRootFragment.kt
@@ -0,0 +1,43 @@
+package com.prevent.feature.setting
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.PreferenceFragmentCompat
+
+class PreferenceRootActivity : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.main_preference, rootKey)
+
+        val preferenceCategory = PreferenceCategory(
+            preferenceScreen.context
+        )
+
+        preferenceScreen.addPreference(preferenceCategory)
+
+        val preference = Preference(
+            preferenceScreen.context
+        ).apply {
+            title = "Title"
+            setOnPreferenceClickListener {
+                Toast.makeText(requireContext(), "title1", Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+
+        preferenceCategory.addPreference(preference)
+
+        val preference2 = Preference(
+            preferenceScreen.context
+        ).apply {
+            title = "Title"
+            setOnPreferenceClickListener {
+                Toast.makeText(requireContext(), "title2", Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+
+        preferenceCategory.addPreference(preference2)
+    }
+}
