2eba41ecda9c8cc00119f59454abf6bab09a6a47
fumiya-kume <fumiya.kume@hotmail.com>
2019-12-18 19:22:58 +0900

    fix build error

--- feature/record/src/main/java/com/prevent/feature/record/RecordModule.kt
+++ feature/record/src/main/java/com/prevent/feature/record/RecordModule.kt
@@ -2,9 +2,10 @@ package com.prevent.feature.record
 
 import com.prevent.feature.record.domain.RecordService
 import com.prevent.feature.record.domain.RecordServiceImpl
+import kotlinx.coroutines.GlobalScope
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val recordModule = module {
-    factory { RecordServiceImpl(androidApplication()) as RecordService }
+    factory { RecordServiceImpl(GlobalScope, androidApplication(), get()) as RecordService }
 }
