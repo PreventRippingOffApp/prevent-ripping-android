59b380c599335d6669e1a2076fc1afec279e5484
fumiya-kume <fumiya.kume@hotmail.com>
2019-12-29 22:48:59 +0900

    add navigate location log activity, when a tap into predict location log cardview

--- feature/record/src/main/java/com/prevent/feature/record/dashboard/RecordDashboardActivity.kt
+++ feature/record/src/main/java/com/prevent/feature/record/dashboard/RecordDashboardActivity.kt
@@ -5,11 +5,13 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.SupportMapFragment
 import com.prevent.feature.record.R
 import com.prevent.feature.record.list.RecordListDialog
 import com.prevent.feature.record.location.LocationLogActivity
 import com.prevent.feature.record.talk.list.TalkListDialog
 import kotlinx.android.synthetic.main.activity_record_dashboard.activity_record_dashboard_alert_with_mail_button
+import kotlinx.android.synthetic.main.activity_record_dashboard.activity_record_dashboard_location_log_card_view
 import kotlinx.android.synthetic.main.activity_record_dashboard.activity_record_dashboard_rip_off_records_material_text_button
 import kotlinx.android.synthetic.main.activity_record_dashboard.activity_record_dashboard_rip_off_talk_material_text_button
 import kotlinx.android.synthetic.main.activity_record_dashboard.activity_record_dashboard_show_all_material_button
@@ -46,22 +48,38 @@ class RecordDashboardActivity : FragmentActivity(R.layout.activity_record_dashbo
                 dialog.show(supportFragmentManager, "")
             }
 
-        activity_record_dashboard_show_all_material_button
-            .setOnClickListener {
-                val transitionOption = ActivityOptionsCompat
-                    .makeSceneTransitionAnimation(
-                        this,
-                        map,
-                        "map"
-                    )
+        activity_record_dashboard_show_all_material_button.setOnClickListener { navigateLocationLogActivity() }
+        activity_record_dashboard_location_log_card_view.setOnClickListener { navigateLocationLogActivity() }
 
-                startActivity(
-                    Intent(
-                        applicationContext,
-                        LocationLogActivity::class.java
-                    ),
-                    transitionOption.toBundle()
-                )
+        // navigate to Location Log Activity, when a reaction to map view
+        (map as SupportMapFragment).getMapAsync {
+            it.setOnMapClickListener {
+                navigateLocationLogActivity()
             }
+            it.setOnCameraMoveStartedListener {
+                navigateLocationLogActivity()
+            }
+            it.setOnMapLongClickListener {
+                navigateLocationLogActivity()
+            }
+        }
+
+    }
+
+    private fun navigateLocationLogActivity() {
+        val transitionOption = ActivityOptionsCompat
+            .makeSceneTransitionAnimation(
+                this,
+                map.view!!,
+                "map"
+            )
+
+        startActivity(
+            Intent(
+                applicationContext,
+                LocationLogActivity::class.java
+            ),
+            transitionOption.toBundle()
+        )
     }
 }
