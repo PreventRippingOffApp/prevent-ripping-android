2c583310fa9dd6cf355c70c0868c893f961b2109
Fumiya Kume <fumiya.kume@hotmail.com>
2019-12-31 14:15:25 +0900

    add monitoring screen (#62)

--- feature/record/src/main/java/com/prevent/feature/record/dashboard/monitoring/MonitoringDashboardActivity.kt
+++ feature/record/src/main/java/com/prevent/feature/record/dashboard/monitoring/MonitoringDashboardActivity.kt
@@ -1,17 +1,29 @@
 package com.prevent.feature.record.dashboard.monitoring
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.prevent.feature.record.R
-import com.prevent.feature.record.databinding.ActivityMonitoringDashboardBinding
+import kotlinx.android.synthetic.main.activity_monitoring_dashboard.activity_monitoring_dashboard_tab_layout
+import kotlinx.android.synthetic.main.activity_monitoring_dashboard.activity_monitoring_dashboard_view_pager_2
 
 class MonitoringDashboardActivity : FragmentActivity(R.layout.activity_monitoring_dashboard) {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        val binding = DataBindingUtil.setContentView<ActivityMonitoringDashboardBinding>(
-            this,
-            R.layout.activity_monitoring_dashboard
-        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val pagerAdapter = MonitoringDashboardPagerAdapter(this)
+        activity_monitoring_dashboard_view_pager_2.adapter = pagerAdapter
+
+        TabLayoutMediator(
+            activity_monitoring_dashboard_tab_layout,
+            activity_monitoring_dashboard_view_pager_2
+        ) { tab: TabLayout.Tab, i: Int ->
+            tab.text = pagerAdapter.fragmentName(i)
+            tab.view.transitionName = pagerAdapter.getTabName(i)
+        }.run {
+            this.attach()
+        }
     }
 }
