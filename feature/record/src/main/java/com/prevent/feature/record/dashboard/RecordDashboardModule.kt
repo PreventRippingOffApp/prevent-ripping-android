8b6f43b6d116f8840e11fb4e0d8d4ca733479c9a
Fumiya Kume <fumiya.kume@hotmail.com>
2020-01-02 17:27:12 +0900

    add record dashboard UI (#55)
    * Feature/vibration (#53)
    
    * add util module
    
    * add vibration permission
    
    * add module refrence
    
    * change permission config
    
    * add vibration logic
    
    * change vibration  duration
    
    * format code
    
    * add record dashboard UI
    
    * add location log screen
    
    * reformat code
    
    * add shared element transition
    
    * add navigate location log activity, when a tap into predict location log cardview
    
    * add help page of ripp off monitoring
    
    * fix code format
    
    * migration "kts" at "data.gradle" (#57)
    
    * migration "kts" at "data.gradle"
    
    * change build config
    
    * remove extra step
    
    * add missing build config for github actions
    
    * fix circleci config
    
    * fix github actions
    
    * fix circleci config
    
    * fix name
    
    * add any build command
    
    * migration "kts" at "data.gradle" (#57)
    
    * migration "kts" at "data.gradle"
    
    * change build config
    
    * remove extra step
    
    * add missing build config for github actions
    
    * fix circleci config
    
    * fix github actions
    
    * fix circleci config
    
    * fix name
    
    * add any build command
    
    * add missing attribute
    
    * change help icon button
    
    * add content description
    
    * change icon position
    
    * add shared element transition
    
    * add monitoring dashboard screen
    
    * update circleci config
    
    * update github actions config
    
    * fix code format
    
    *  add walkthrough (#60)
    
    * add walkthrough module
    
    * add walkthrough
    
    * add talk fragment
    
    * add talk list fragment
    
    * add monitoring screen (#62)
    
    * Fix/remove extra ci config (#64)
    
    * remove extra circleci config
    
    * remove extra github actions config
    
    * fix job name
    
    * fix workflow define
    
    * fix reference job name
    
    * add email icon (#65)
    
    * add local bar icon (#69)
    
    * add help icon (#67)
    
    * add my location icon (#68)
    
    * add my location icon
    
    * add email icon (#65)
    
    * add local bar icon (#69)
    
    * add help icon (#67)
    
    * add app bar height dimen (#70)
    
    *  add walkthrough (#60)
    
    * add walkthrough module
    
    * add walkthrough
    
    * add monitoring screen (#62)
    
    * Fix/remove extra ci config (#64)
    
    * remove extra circleci config
    
    * remove extra github actions config
    
    * fix job name
    
    * fix workflow define
    
    * fix reference job name
    
    * fix build error
    
    * add record dashboard UI
    
    * migration "kts" at "data.gradle" (#57)
    
    * migration "kts" at "data.gradle"
    
    * change build config
    
    * remove extra step
    
    * add missing build config for github actions
    
    * fix circleci config
    
    * fix github actions
    
    * fix circleci config
    
    * fix name
    
    * add any build command
    
    * update github actions config
    
    *  add walkthrough (#60)
    
    * add walkthrough module
    
    * add walkthrough
    
    * Fix/remove extra ci config (#64)
    
    * remove extra circleci config
    
    * remove extra github actions config
    
    * fix job name
    
    * fix workflow define
    
    * fix reference job name
    
    * Fix/remove extra ci config (#64)
    
    * remove extra circleci config
    
    * remove extra github actions config
    
    * fix job name
    
    * fix workflow define
    
    * fix reference job name
    
    * fix build error

---
+++ feature/record/src/main/java/com/prevent/feature/record/dashboard/RecordDashboardModule.kt
@@ -0,0 +1,1 @@
+package com.prevent.feature.record.dashboard
---
+++ feature/record/src/main/java/com/prevent/feature/record/location/LocationLogActivity.kt
@@ -0,0 +1,30 @@
+package com.prevent.feature.record.location
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.transition.TransitionInflater
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import com.prevent.feature.record.R
+import com.prevent.feature.record.databinding.ActivityLocationLogBinding
+
+class LocationLogActivity : FragmentActivity(R.layout.activity_location_log) {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        val binding = DataBindingUtil.setContentView<ActivityLocationLogBinding>(
+            this,
+            R.layout.activity_location_log
+        )
+
+        binding.map.transitionName = "map"
+
+        window.sharedElementEnterTransition = TransitionInflater.from(applicationContext)
+            .inflateTransition(R.transition.activity_location_log_transition)
+    }
+
+    override fun onBackPressed() {
+        ActivityCompat.finishAfterTransition(this)
+    }
+}
