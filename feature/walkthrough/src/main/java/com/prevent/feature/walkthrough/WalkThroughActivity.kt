a3fe249b3de520cf94f01f89bab273af50f22084
Fumiya Kume <fumiya.kume@hotmail.com>
2020-01-08 08:29:04 +0900

    remove databinding on walkthrough (#105)

--- feature/walkthrough/src/main/java/com/prevent/feature/walkthrough/WalkThroughActivity.kt
+++ feature/walkthrough/src/main/java/com/prevent/feature/walkthrough/WalkThroughActivity.kt
@@ -2,10 +2,13 @@ package com.prevent.feature.walkthrough
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
-import com.prevent.feature.walkthrough.databinding.ActivityWalkthroughBinding
+import com.prevent.data.flags.Flags
+import kotlinx.android.synthetic.main.activity_walkthrough.activity_walkthrough_finish_button
+import kotlinx.android.synthetic.main.activity_walkthrough.activity_walkthrough_progress_bar
+import kotlinx.android.synthetic.main.activity_walkthrough.activity_walkthrough_view_pager_2
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -13,20 +16,17 @@ class WalkThroughActivity : FragmentActivity(R.layout.activity_walkthrough) {
 
     private val walkthroughNavigator: WalkthroughNavigator by inject()
     private val walkThroughViewModel: WalkThroughViewModel by viewModel()
+    private val flags: Flags by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityWalkthroughBinding>(
-            this,
-            R.layout.activity_walkthrough
-        )
-
         val pageradapter = WalkThroughPagerAdapter(this) {
-            binding.activityWalkthroughViewPager2.currentItem =
-                binding.activityWalkthroughViewPager2.currentItem + 1
+            activity_walkthrough_view_pager_2.currentItem =
+                activity_walkthrough_view_pager_2.currentItem + 1
         }
-        binding.activityWalkthroughViewPager2.registerOnPageChangeCallback(object :
+
+        activity_walkthrough_view_pager_2.registerOnPageChangeCallback(object :
             ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
@@ -34,17 +34,25 @@ class WalkThroughActivity : FragmentActivity(R.layout.activity_walkthrough) {
                     pageradapter,
                     position
                 )
+                activity_walkthrough_finish_button.visibility = if (position == 2) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+
                 // disable on input user information page.
-                binding.activityWalkthroughViewPager2.isUserInputEnabled = position != 1
+                activity_walkthrough_view_pager_2.isUserInputEnabled = position != 1
             }
         })
-        binding.activityWalkthroughFinishButton.setOnClickListener {
-            val mainScreen = walkthroughNavigator.getMainScreen()
-            val intent = Intent(applicationContext, mainScreen.javaClass)
-            startActivity(
-                intent
-            )
-        }
+        activity_walkthrough_finish_button
+            .setOnClickListener {
+                flags.walkthroughShowed = true
+                val mainScreen = walkthroughNavigator.getMainScreen()
+                val intent = Intent(applicationContext, mainScreen.javaClass)
+                startActivity(
+                    intent
+                )
+            }
 
         walkThroughViewModel.pageChanged(
             pageradapter,
@@ -53,8 +61,8 @@ class WalkThroughActivity : FragmentActivity(R.layout.activity_walkthrough) {
         walkThroughViewModel
             .walkThroughLiveData
             .observeForever {
-                binding.viewEntity = it
+                activity_walkthrough_progress_bar.progress = it.progress
             }
-        binding.activityWalkthroughViewPager2.adapter = pageradapter
+        activity_walkthrough_view_pager_2.adapter = pageradapter
     }
 }
