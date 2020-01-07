b1478420b8a0d28959319679788d518e1e8f5bd7
fumiya-kume <fumiya.kume@hotmail.com>
2020-01-07 22:31:30 +0900

    remove walkthrough flags

--- app/src/main/java/com/prevent/ripping/MainActivity.kt
+++ app/src/main/java/com/prevent/ripping/MainActivity.kt
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (flags.walkthroughShowed.not()) {
-            flags.walkthroughShowed = true
-
             showWalkthrough()
         }
     }
